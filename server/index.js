4cb6ef7b90d46d65ac76a5bf50c7f16013de9421
kush <ujjwalkaushik24@gmail.com>
2024-10-24 20:23:46 +0530

    Overview Page

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ import User from "./models/User.js";
 import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js";
+import OverallStat from './models/OverallStat.js';
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -52,6 +53,7 @@ mongoose.connect(process.env.MONGO_URL, {
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
+    // OverallStat.insertMany(dataOverallStat);
 }).catch((error) => {
     console.error(`${error} did not connect`)
 })
