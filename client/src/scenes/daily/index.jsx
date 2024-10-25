34121621c7f93402d3ccd55720c303745587c3d0
kush <ujjwalkaushik24@gmail.com>
2024-10-25 20:40:39 +0530

    Chart Issue Fixed - https://github.com/ujjwalkaushik/mern-reduxtoolkit/issues/5

--- client/src/scenes/daily/index.jsx
+++ client/src/scenes/daily/index.jsx
@@ -44,7 +44,7 @@ const Daily = () => {
     });
 
     const formattedData = [totalSalesLine, totalUnitsLine];
-    console.log(formattedData)
+    console.log(formattedData);
     return [formattedData];
   }, [data, startDate, endDate]); // eslint-disable-line react-hooks/exhaustive-deps
 
@@ -75,108 +75,110 @@ const Daily = () => {
         </Box>
 
         {data ? (
-          <ResponsiveLine
-            data={formattedData}
-            theme={{
-              axis: {
-                domain: {
-                  line: {
-                    stroke: theme.palette.secondary[200],
-                  },
-                },
-                legend: {
-                  text: {
-                    fill: theme.palette.secondary[200],
-                  },
-                },
-                ticks: {
-                  line: {
-                    stroke: theme.palette.secondary[200],
-                    strokeWidth: 1,
-                  },
-                  text: {
-                    fill: theme.palette.secondary[200],
-                  },
-                },
-              },
-              legends: {
-                text: {
-                  fill: theme.palette.secondary[200],
-                },
-              },
-              tooltip: {
-                container: {
-                  color: theme.palette.primary.main,
-                },
-              },
-            }}
-            colors={{ datum: "color" }}
-            margin={{ top: 50, right: 50, bottom: 70, left: 60 }}
-            xScale={{ type: "point" }}
-            yScale={{
-              type: "linear",
-              min: "auto",
-              max: "auto",
-              stacked: false,
-              reverse: false,
-            }}
-            yFormat=" >-.2f"
-            curve="catmullRom"
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-              orient: "bottom",
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 90,
-              legend: "Month",
-              legendOffset: 60,
-              legendPosition: "middle",
-            }}
-            axisLeft={{
-              orient: "left",
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 0,
-              legend: "Total",
-              legendOffset: -50,
-              legendPosition: "middle",
-            }}
-            enableGridX={false}
-            enableGridY={false}
-            pointSize={10}
-            pointColor={{ theme: "background" }}
-            pointBorderWidth={2}
-            pointBorderColor={{ from: "serieColor" }}
-            pointLabelYOffset={-12}
-            useMesh={true}
-            legends={[
-              {
-                anchor: "top-right",
-                direction: "column",
-                justify: false,
-                translateX: 50,
-                translateY: 0,
-                itemsSpacing: 0,
-                itemDirection: "left-to-right",
-                itemWidth: 80,
-                itemHeight: 20,
-                itemOpacity: 0.75,
-                symbolSize: 12,
-                symbolShape: "circle",
-                symbolBorderColor: "rgba(0, 0, 0, .5)",
-                effects: [
-                  {
-                    on: "hover",
-                    style: {
-                      itemBackground: "rgba(0, 0, 0, .03)",
-                      itemOpacity: 1,
+          <>
+            <ResponsiveLine
+              data={formattedData}
+              theme={{
+                axis: {
+                  domain: {
+                    line: {
+                      stroke: theme.palette.secondary[200],
                     },
                   },
-                ],
-              },
-            ]}
-          />
+                  legend: {
+                    text: {
+                      fill: theme.palette.secondary[200],
+                    },
+                  },
+                  ticks: {
+                    line: {
+                      stroke: theme.palette.secondary[200],
+                      strokeWidth: 1,
+                    },
+                    text: {
+                      fill: theme.palette.secondary[200],
+                    },
+                  },
+                },
+                legends: {
+                  text: {
+                    fill: theme.palette.secondary[200],
+                  },
+                },
+                tooltip: {
+                  container: {
+                    color: theme.palette.primary.main,
+                  },
+                },
+              }}
+              colors={{ datum: "color" }}
+              margin={{ top: 50, right: 50, bottom: 70, left: 60 }}
+              xScale={{ type: "point" }}
+              yScale={{
+                type: "linear",
+                min: "auto",
+                max: "auto",
+                stacked: false,
+                reverse: false,
+              }}
+              yFormat=" >-.2f"
+              curve="catmullRom"
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                orient: "bottom",
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 90,
+                legend: "Month",
+                legendOffset: 60,
+                legendPosition: "middle",
+              }}
+              axisLeft={{
+                orient: "left",
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: "Total",
+                legendOffset: -50,
+                legendPosition: "middle",
+              }}
+              enableGridX={false}
+              enableGridY={false}
+              pointSize={10}
+              pointColor={{ theme: "background" }}
+              pointBorderWidth={2}
+              pointBorderColor={{ from: "serieColor" }}
+              pointLabelYOffset={-12}
+              useMesh={true}
+              legends={[
+                {
+                  anchor: "top-right",
+                  direction: "column",
+                  justify: false,
+                  translateX: 50,
+                  translateY: 0,
+                  itemsSpacing: 0,
+                  itemDirection: "left-to-right",
+                  itemWidth: 80,
+                  itemHeight: 20,
+                  itemOpacity: 0.75,
+                  symbolSize: 12,
+                  symbolShape: "circle",
+                  symbolBorderColor: "rgba(0, 0, 0, .5)",
+                  effects: [
+                    {
+                      on: "hover",
+                      style: {
+                        itemBackground: "rgba(0, 0, 0, .03)",
+                        itemOpacity: 1,
+                      },
+                    },
+                  ],
+                },
+              ]}
+            />
+          </>
         ) : (
           <>Loading...</>
         )}
