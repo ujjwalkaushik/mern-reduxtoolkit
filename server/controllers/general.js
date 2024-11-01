456709055a44cca32f5f336e945466c015032b24
kush <ujjwalkaushik24@gmail.com>
2024-11-01 21:17:10 +0530

    node module removed from project directory

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -2,13 +2,41 @@ import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
 import OverallStat from "../models/OverallStat.js";
 
+import { Parser } from "json2csv";
+
+export const downloadUserStats = async (req, res, next) => {
+  try {
+    const currentMonth = "November";
+    const currentYear = 2021;
+    const currentDay = "2021-11-15";
+
+    const overallStat = await OverallStat.find({
+      year: currentYear,
+      "monthlyData.month": currentMonth,
+      "dailyData.date": currentDay,
+    }).lean();
+
+    const fields = ["totalCustomers", "yearlySalesTotal", "year"];
+
+    const json2csvParser = new Parser({ fields });
+    const csv = json2csvParser.parse(overallStat);
+    // Set headers to download as CSV file
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader("Content-Disposition", 'attachment; filename="report.csv"');
+
+    // Send the CSV data
+    res.status(200).send(csv);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const getUser = async (req, res, next) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
     res.status(200).json(user);
   } catch (error) {
-    console.log(error);
     next(error);
   }
 };
